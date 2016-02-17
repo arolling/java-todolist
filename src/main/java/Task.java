6fc06248d12ca2fbc3ab6019c41683b503bb09e8
Abby <arolling@gmail.com>
2016-02-16 21:48:15 -0800

    Add completion status and created date to task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,26 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Task {
   String mDescription;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
 
   public Task(String description) {
     mDescription = description;
+    mCompleted = false;
   }
 
   public String getDescription() {
     return mDescription;
+  }
+
+  public boolean isCompleted(){
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    mCreatedAt = LocalDateTime.now();
+    return mCreatedAt;
   }
 }
