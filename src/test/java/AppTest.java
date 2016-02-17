54cea1659086bcf4484a5e0e89fb7240b84b0e6c
Abby <arolling@gmail.com>
2016-02-16 22:55:48 -0800

    Finish improvements as per video

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
@@ -19,6 +18,9 @@ public class AppTest extends FluentTest {
     return webDriver;
   }
 
+  @Rule
+  public ClearRule clearRule = new ClearRule();
+
   @ClassRule
   public static ServerRule server = new ServerRule();
   // Unit testing
@@ -27,12 +29,13 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("Add a new Task");
+    assertThat(pageSource()).contains("Todo list!");
   }
 
   @Test
   public void taskIsCreatedTest() {
     goTo("http://localhost:4567/");
+    click("a", withText("Add a new task"));
     fill("#description").with("Mow the lawn");
     submit(".btn");
     assertThat(pageSource()).contains("Your task has been saved.");
@@ -40,23 +43,38 @@ public class AppTest extends FluentTest {
 
   @Test
   public void taskIsDisplayedTest() {
-    goTo("http://localhost:4567/");
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Mow the lawn");
     submit(".btn");
-    click("a", withText("Go Back"));
+    click("a", withText("View tasks"));
     assertThat(pageSource()).contains("Mow the lawn");
   }
 
   @Test
   public void multipleTasksAreDisplayedTest() {
-    goTo("http://localhost:4567/");
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Mow the lawn");
     submit(".btn");
-    click("a", withText("Go Back"));
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Buy groceries");
     submit(".btn");
-    click("a", withText("Go Back"));
+    click("a", withText("View tasks"));
     assertThat(pageSource()).contains("Mow the lawn");
     assertThat(pageSource()).contains("Buy groceries");
+  }
+
+  @Test
+  public void taskShowPageDisplaysDescription() {
+    goTo("http://localhost:4567/tasks/new");
+    fill("#description").with("Do the dishes");
+    submit(".btn");
+    click("a", withText("View tasks"));
+    click("a", withText("Do the dishes"));
+    assertThat(pageSource()).contains("Do the dishes");
+  }
+  @Test
+  public void taskNotFoundMessageShown() {
+    goTo("http://localhost:4567/tasks/999");
+    assertThat(pageSource()).contains("Task not found");
   }
 }
